05946761f6edd993cd94a2c15cd1082fd406fac9
Josh Shepherd <josh_lmao@live.co.uk>
2019-10-21 16:21:51 +0100

    Added more rooms, floor 2

--- Octo-Spork/Octo-Spork/Main.cpp
+++ Octo-Spork/Octo-Spork/Main.cpp
@@ -53,12 +53,14 @@ void PlayGame()
 		DisplayInfo("What will you do?");
 		Input::Instruction inst = Input::ReadUser();
 		UpdateState(inst);
+		InfoBuffer();
 	}
 }
 
 /* Creates the buildings rooms, items, etc */
 void InitBuilding()
 {
+	// Floor 1
 	Room mainHall = Room("Main-Hall", "Where I woke up. There's a massive sign on the wall with it's name");
 	mainHall.Exits = { "North-Hallway", "West-Hallway", "Staircase" };
 
@@ -68,6 +70,13 @@ void InitBuilding()
 	Room eHallway = Room("East-Hallway", "Hidden in the back of the complex");
 	eHallway.Exits = { "North-Hallway", "MI035" };
 
+	Room sHallway = Room("South-Hallway", "");
+	sHallway.Exits = { "East-Hallway", "West-Hallway" };
+
+	Room wHallway = Room("West-Hallway", "");
+	wHallway.Exits= { "South-Hallway", "Main-Hall" };
+
+	// Floor 1 - Rooms
 	Room mi034 = Room("MI034", "A room");
 	Item mouse = Item("Mouse");
 	mi034.SetItem(mouse);
@@ -75,6 +84,30 @@ void InitBuilding()
 	Room mi035 = Room("MI035", "Description");
 	Item candle = Item("Candle");
 	mi035.SetItem(candle);
+
+	// Floor 1 to 2 Staircase 
+	Room staircase = Room("Staircase", "");
+	staircase.Exits = { "Main-Hall", "F2-Landing" };
+
+	// Floor 2
+	Room f2Landing = Room("F2-Landing", "");
+	f2Landing.Exits = { "Staircase", "F2-North-Hallway" };
+
+	Room f2nHallway = Room("F2-North-Hallway", "");
+	f2nHallway.Exits = { "F2-East-Hallway", "F2-Landing", "MI102" };
+
+	Room f2eHallway = Room("F2-East-Hallway", "");
+	f2eHallway.Exits = { "F2-North-Hallway", "F2-East-Hallway" };
+
+	Room f2sHallway = Room("F2-South-Hallway", "");
+	f2sHallway.Exits = { "F2-East-Hallway", "F2-North-Hallway" };
+
+	// Floor 2 - Rooms
+	Room mi102a = Room("MI102a", "");
+	mi102a.Exits = { "F2-Landing" };
+
+	Room mi102c = Room("MI102c", "");
+	mi102c.Exits = { "F2-North-Hallway" };
 
 	AllRooms = {
 		mainHall, nHallway, eHallway, mi034, mi035
